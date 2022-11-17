813d68dd1878587d6e89fc9995cab35a357542dc
Glyke Kinney P. Alivio <glykekinney.alivio@cit.edu>
2022-11-17 16:03:41 +0800

    Refactor CourseEntity model

--- cats/src/main/java/com/internetlove/cats/Entity/CourseEntity.java
+++ cats/src/main/java/com/internetlove/cats/Entity/CourseEntity.java
@@ -2,37 +2,45 @@ package com.internetlove.cats.Entity;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tbl_course")
+@Table(name = "course")
 public class CourseEntity {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
-	private int id; //Auto-incremented id of the Course
+	private int id;
+	
 	@Column(name = "course_code")
 	private String code;
+	
 	@Column(name = "course_description")
 	private String description;
+	
 	//private Set<TeacherEntity> teachers; //To be implemented
-	//private Set<StudentEntity> students; //To be implemented
+	
+	@OneToMany(cascade = CascadeType.MERGE)
+	private Set<StudentEntity> students;
 	
 	public CourseEntity() {}
-	public CourseEntity(int cid, String ccode, String cdesc) {
+	public CourseEntity(int id, String code, String description) {
 		super();
-		id = cid;
-		code = ccode;
-		description = cdesc;
+		this.id = id;
+		this.code = code;
+		this.description = description;
 	}
 	
 	public String getCourseCode() {return code;}
 	public String getCourseDesc() {return description;}
-	public void setCourseCode(String ccode) {code = ccode;}
-	public void setCourseDesc(String cdesc) {description = cdesc;}
+	public Set<StudentEntity> getStudents() {return students;}
+	public void setCourseCode(String code) {this.code = code;}
+	public void setCourseDesc(String desc) {this.description = desc;}
 }
