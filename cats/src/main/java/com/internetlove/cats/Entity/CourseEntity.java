0cd2e48735a2dcdd68942d2169feca6d27da00c8
Glyke Kinney P. Alivio <glykekinney.alivio@cit.edu>
2022-11-17 15:39:25 +0800

    Modify CourseEntity

--- cats/src/main/java/com/internetlove/cats/Entity/CourseEntity.java
+++ cats/src/main/java/com/internetlove/cats/Entity/CourseEntity.java
@@ -2,17 +2,23 @@ package com.internetlove.cats.Entity;
 
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tbl_course")
 public class CourseEntity {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id; //Auto-incremented id of the Course
+	@Column(name = "course_code")
 	private String code;
+	@Column(name = "course_description")
 	private String description;
 	//private Set<TeacherEntity> teachers; //To be implemented
 	//private Set<StudentEntity> students; //To be implemented
