57231ad295d753edb15f28b6a8ffe6c2d0d4dbf8
JAY D. NAYON JR <jay.nayonjr@cit.edu>
2022-12-19 13:39:22 +0800

    Fix bugs from recent checkout

--- cats/src/main/java/com/internetlove/cats/Controller/UniversityController.java
+++ cats/src/main/java/com/internetlove/cats/Controller/UniversityController.java
@@ -3,6 +3,7 @@ package com.internetlove.cats.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.internetlove.cats.Service.UniversityService;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/university")
 public class UniversityController {
